88ecc233e52989568ef4d9414e5caeadc620c46f
jprchlik <japrchlik@gmail.com>
2016-12-08 11:45:35 -0500

    dark_image class completes. Most useful parameters are lev0fil which is a list of level0 dark files

--- dark_images.py
+++ dark_images.py
@@ -5,46 +5,59 @@ import numpy as np
 
 
 
-class dark_data
+class dark_data:
 
 
     def __init__(self,start='2012/01/01',end='3001/01/01',
                  lev1dir='/data/alisdair/IRIS_LEVEL1_DARKS/',
-                 lev0dir='data/alisdair/opabina/scratch/joan/iris/newdat/orbit/level0/',
-                 typed='simpleB',clobber=False)
+                 lev0dir='/data/alisdair/opabina/scratch/joan/iris/newdat/orbit/level0/',
+                 typed='simpleB',clobber=False):
+
 
         start,end = start.split('/'),end.split('/')
 
-        self.start = datetime(start[0],start[1],start[2])
-        self.end   = datetime(end[0],end[1],end[2])
+        self.start = datetime(int(start[0]),int(start[1]),int(start[2]))
+        self.end   = datetime(int(end[0]  ),int(end[1]  ),int(end[2]  ))
 
-        self.lev1dir = lev1dir
-        self.lev0dir = lev0dir
-  
         self.typed = typed
+        self.lev1dir = lev1dir
+        self.lev0dir = lev0dir+self.typed+'/'
+  
+
+        self.get_subdirectories()
+        self.cut_subdir_range()
+        self.get_file_list()
 
 
-    def get_subdirectorys(self):
-        yeard = glob.glob(self.lev1dir)
-        montd = [glob.glob(i) for i in yeard]
+    def get_subdirectories(self):
+        yeard = glob.glob(self.lev0dir+'*')
+        monthd = [glob.glob(i+'/*') for i in yeard]
+
+#make the array 1d
+        monthd = np.ravel(monthd)
 
         self.lev1mons = []
         self.lev0mons = []
         self.darktime = []
 #create subdirectory list
-        for i in monthd:
-            self.lev1mons.append(i+'/'+self.typed)
-            self.lev0mons.append(i.replace(self.lev1dir,self.lev0dir)+'/'+self.typed))
-            self.darktime.append(datetime(int(i.split('/')[-2]),int(i.split('/')[-1]),1))
+        for j in monthd:
+            for i in j:
+                self.lev1mons.append(i.replace(self.lev0dir,self.lev1dir)+'/'+self.typed)
+                self.lev0mons.append(i+'/')
+                self.darktime.append(datetime(int(i.split('/')[-2]),int(i.split('/')[-1]),1))
 
 #Only keep subdirectories in a specific range
     def cut_subdir_range(self):
 
+        self.darktime = np.array(self.darktime)
+        self.lev1mons = np.array(self.lev1mons)
+        self.lev0mons = np.array(self.lev0mons)
+
         keep, = np.where((self.darktime >= self.start) & (self.darktime <= self.end))
 
-        self.darktime = np.array(self.darktime)[keep]
-        self.lev1mons = np.array(self.lev1mons)[keep]
-        self.lev0mons = np.array(self.lev0mons)[keep]
+        self.darktime = self.darktime[keep]
+        self.lev1mons = self.lev1mons[keep]
+        self.lev0mons = self.lev0mons[keep]
    
         
 
@@ -53,7 +66,10 @@ class dark_data
 
         self.lev0fil = []
         self.lev1fil = []
-        for i,j enumerate(self.lev0mons):
+        for i,j in enumerate(self.lev0mons):
             k = self.lev1mons[i]
+
+            for p in glob.glob(j+'/*fits'): self.lev0fil.append(p)
+            for p in glob.glob(k+'/*fits'): self.lev1fil.append(p)
 
 
