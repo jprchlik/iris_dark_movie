0ef47ca246f4bb7f7178217590ad3451e8208501
jprchlik <japrchlik@gmail.com>
2016-12-08 14:05:59 -0500

    Added multiprocessing and movie creation to script file

--- dark_movie_script.py
+++ dark_movie_script.py
@@ -1,11 +1,30 @@
 from dark_images import dark_files
 from make_dark_images import dark_plot
+from multiprocessing import Pool
+from make_movie import create_movie
 
+itype = 'NUV'
 def run_images(ifile):
 
-    dark_plot(ifile,clobber=True)
+    out =dark_plot(ifile,clobber=False,itype=itype,restrict=['EXPTIME:0'])
+    
+    return out.ofile
 
 
 darks = dark_files()
 
-run_images(darks.lev0fil[-14])
+nproc = 8
+
+fdark = darks.lev0fil
+
+pool = Pool(processes=nproc)
+ofiles = pool.map(run_images,fdark)
+pool.close()
+
+
+
+#ofiles = run_images(fdark)
+
+
+imov = create_movie(ofiles,outmov=itype+'_dark_movie.mp4')
+imov.create_movie()
